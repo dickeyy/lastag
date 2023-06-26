8609be76685a27b104a4ccfa8b687f032d9cc3ec
ZerohRaG3 <kyledickey02@gmail.com>
2023-06-26 00:23:46 -0600

    Update clerk-webhook.ts

--- src/pages/api/auth/clerk-webhook.ts
+++ src/pages/api/auth/clerk-webhook.ts
@@ -22,12 +22,6 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
     const wh = new Webhook(secret);
     let msg: any;
 
-    // set the webhook event 
-    const evt = msg as WebhookEvent; 
-
-    console.log(msg)
-    console.log(evt)
-    console.log(payload)
 
     // verify the webhook
     try {
@@ -36,58 +30,14 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
         res.status(400).json({});
     }
 
-    switch (evt.type) {
-        // User created 
-        case 'user.created':
-            
-            // create the user doc
-            db.collection("users").insertOne({
-                id: evt.data.id,
-                email: evt.data.email_addresses[0].email_address,
-                username: evt.data.username,
-                pfp: evt.data.profile_image_url,
-            }).then(() => {
+    // set the webhook event 
+    const evt = msg as WebhookEvent; 
 
-                // create the tag doc
-                db.collection("tags").insertOne({
-
-                    id: evt.data.id,
-                    tag_name: evt.data.username,
-                    tags: [],
-                    socials: [],
-                    icon: evt.data.profile_image_url,
-                    bio: "",
-                    style: {
-                        theme: "dracula",
-                        background: "#272935",
-                        color: "#fff",
-                        accent: "#ff7ac6",
-                        tag_bg: "#16171d",
-                        tag_title: "#fff",
-                        tag_desc: "#fff",
-                        social: "#fff",
-                        social_hover: "#ff7ac6",
-                    }
-
-                }).then(() => {
-                    client.close()
-                    return res.status(200).send("ok")
-                }).catch((err) => {
-                    console.log(err)
-                    return res.status(500).send(err)
-                })
-            }).catch((err) => {
-                console.log(err)
-                return res.status(500).send(err)
-            })
-            break;
-        
-        // user updated
-        case 'user.updated':
-            // delete the user doc
-            db.collection("users").deleteOne({
-                id: evt.data.id
-            }).then(() => {
+    if (evt) {
+        switch (evt.type) {
+            // User created 
+            case 'user.created':
+                
                 // create the user doc
                 db.collection("users").insertOne({
                     id: evt.data.id,
@@ -95,16 +45,67 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
                     username: evt.data.username,
                     pfp: evt.data.profile_image_url,
                 }).then(() => {
-                    client.close()
-                    return res.status(200).send("ok")
+
+                    // create the tag doc
+                    db.collection("tags").insertOne({
+
+                        id: evt.data.id,
+                        tag_name: evt.data.username,
+                        tags: [],
+                        socials: [],
+                        icon: evt.data.profile_image_url,
+                        bio: "",
+                        style: {
+                            theme: "dracula",
+                            background: "#272935",
+                            color: "#fff",
+                            accent: "#ff7ac6",
+                            tag_bg: "#16171d",
+                            tag_title: "#fff",
+                            tag_desc: "#fff",
+                            social: "#fff",
+                            social_hover: "#ff7ac6",
+                        }
+
+                    }).then(() => {
+                        client.close()
+                        return res.status(200).send("ok")
+                    }).catch((err) => {
+                        console.log(err)
+                        return res.status(500).send(err)
+                    })
                 }).catch((err) => {
                     console.log(err)
                     return res.status(500).send(err)
                 })
-            }).catch((err) => {
-                console.log(err)
-                return res.status(500).send(err)
-            })
-            break;
+                break;
+            
+            // user updated
+            case 'user.updated':
+                // delete the user doc
+                db.collection("users").deleteOne({
+                    id: evt.data.id
+                }).then(() => {
+                    // create the user doc
+                    db.collection("users").insertOne({
+                        id: evt.data.id,
+                        email: evt.data.email_addresses[0].email_address,
+                        username: evt.data.username,
+                        pfp: evt.data.profile_image_url,
+                    }).then(() => {
+                        client.close()
+                        return res.status(200).send("ok")
+                    }).catch((err) => {
+                        console.log(err)
+                        return res.status(500).send(err)
+                    })
+                }).catch((err) => {
+                    console.log(err)
+                    return res.status(500).send(err)
+                })
+                break;
+        }
+    } else {
+        return res.status(400).send("invalid event")
     }
 }
