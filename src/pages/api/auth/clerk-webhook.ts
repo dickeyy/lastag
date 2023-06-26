b354163405e78cf01145de1f627a63d8ccd8af72
ZerohRaG3 <kyledickey02@gmail.com>
2023-06-26 00:32:49 -0600

    Update clerk-webhook.ts

--- src/pages/api/auth/clerk-webhook.ts
+++ src/pages/api/auth/clerk-webhook.ts
@@ -103,6 +103,30 @@ export default async function (req: NextApiRequest, res: NextApiResponse) {
                     return res.status(500).send(err)
                 })
                 break;
+            
+            // user deleted
+            case 'user.deleted':
+                // delete the user doc
+                db.collection("users").deleteOne({
+                    id: evt.data.id
+                }).then(() => {
+                    // delete the tag doc
+                    db.collection("tags").deleteOne({
+                        id: evt.data.id
+                    }).then(() => {
+                        client.close()
+                        return res.status(200).send("ok")
+                    }
+                    ).catch((err) => {
+                        console.log(err)
+                        return res.status(500).send(err)
+                    }
+                    )
+                }).catch((err) => {
+                    console.log(err)
+                    return res.status(500).send(err)
+                })
+                break;
         }
     } else {
         return res.status(400).send("invalid event")
